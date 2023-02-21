03e770c47501ba96399e439fea00bb04108ae1e6
Josh <jbrewer@weioffice.com>
2023-02-21 08:30:26 -0600

    trouble config, small lsp changes

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -50,12 +50,16 @@ return {
 		},
 		config = function()
 			local on_attach = function(_, _)
+				local opts = { noremap = true, silent = true }
+
 				vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, {})
+				vim.keymap.set("n", "<leader>of", vim.diagnostic.open_float, opts)
 				vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
 				vim.keymap.set("n", "gr", require("telescope.builtin").lsp_references, {})
 				vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
 				vim.keymap.set("n", "gi", vim.lsp.buf.implementation, {})
 				vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+				vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition, bufopts)
 			end
 
 			local capabilities = require("cmp_nvim_lsp").default_capabilities()
