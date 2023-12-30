6ca4170465eace1083be61091c21418f4f0aa102
Joshbwr <josh.brewer90@gmail.com>
2023-12-30 13:20:31 -0600

    node-debugger

--- lua/josh/plugins/dap.lua
+++ lua/josh/plugins/dap.lua
@@ -1,124 +1,133 @@
 return {
-  {
-    "rcarriga/nvim-dap-ui",
-    dependencies = {
-      "jay-babu/mason-nvim-dap.nvim",
-      "mfussenegger/nvim-dap",
-      "mxsdev/nvim-dap-vscode-js",
-      "williamboman/mason.nvim",
-      "leoluz/nvim-dap-go",
+  "mfussenegger/nvim-dap",
+  config = function() end,
+
+  dependencies = {
+
+    -- fancy UI for the debugger
+    {
+      "rcarriga/nvim-dap-ui",
+      -- stylua: ignore
+      keys = {
+        { "<leader>du", function() require("dapui").toggle({ }) end, desc = "Dap UI" },
+        { "<leader>de", function() require("dapui").eval() end, desc = "Eval", mode = {"n", "v"} },
+      },
+      opts = {},
+      config = function(_, opts)
+        -- setup dap config by VsCode launch.json file
+        -- require("dap.ext.vscode").load_launchjs()
+        local dap = require("dap")
+        local dapui = require("dapui")
+        dapui.setup(opts)
+        dap.listeners.after.event_initialized["dapui_config"] = function()
+          dapui.open({})
+        end
+        dap.listeners.before.event_terminated["dapui_config"] = function()
+          dapui.close({})
+        end
+        dap.listeners.before.event_exited["dapui_config"] = function()
+          dapui.close({})
+        end
+      end,
     },
-    config = function()
-      local mason_dap = require("mason-nvim-dap")
-      local dap = require("dap")
-      local ui = require("dapui")
 
-      dap.set_log_level("TRACE")
-      mason_dap.setup({
-        ensure_installed = { "delve" },
-        automatic_installation = true,
-      })
+    -- virtual text for the debugger
+    {
+      "theHamsta/nvim-dap-virtual-text",
+      opts = {},
+    },
 
-      dap.configurations = {
-        go = {
-          {
-            type = "go", -- Which adapter to use
-            name = "Debug", -- Human readable name
-            request = "launch", -- Whether to "launch" or "attach" to program
-            program = "${file}", -- The buffer you are focused on when running nvim-dap
-          },
+    -- which key integration
+    {
+      "folke/which-key.nvim",
+      optional = true,
+      opts = {
+        defaults = {
+          ["<leader>d"] = { name = "+debug" },
         },
-      }
+      },
+    },
 
-      dap.adapters.go = {
+    -- mason.nvim integration
+    {
+      "jay-babu/mason-nvim-dap.nvim",
+      dependencies = "mason.nvim",
+      cmd = { "DapInstall", "DapUninstall" },
+      opts = {
+        -- Makes a best effort to setup the various debuggers with
+        -- reasonable debug configurations
+        automatic_installation = true,
+
+        -- You can provide additional configuration to the handlers,
+        -- see mason-nvim-dap README for more information
+        handlers = {},
+
+        -- You'll need to check that you have the required things installed
+        -- online, please don't ask me how to install them :)
+        ensure_installed = {
+          -- Update this to ensure that you have the debuggers for the langs you want
+        },
+      },
+    },
+  },
+
+  -- stylua: ignore
+  keys = {
+    { "<leader>dB", function() require("dap").set_breakpoint(vim.fn.input('Breakpoint condition: ')) end, desc = "Breakpoint Condition" },
+    { "<leader>db", function() require("dap").toggle_breakpoint() end, desc = "Toggle Breakpoint" },
+    { "<leader>dc", function() require("dap").continue() end, desc = "Continue" },
+    { "<leader>da", function() require("dap").continue({ before = get_args }) end, desc = "Run with Args" },
+    { "<leader>dC", function() require("dap").run_to_cursor() end, desc = "Run to Cursor" },
+    { "<leader>dg", function() require("dap").goto_() end, desc = "Go to line (no execute)" },
+    { "<leader>di", function() require("dap").step_into() end, desc = "Step Into" },
+    { "<leader>dj", function() require("dap").down() end, desc = "Down" },
+    { "<leader>dk", function() require("dap").up() end, desc = "Up" },
+    { "<leader>dl", function() require("dap").run_last() end, desc = "Run Last" },
+    { "<leader>do", function() require("dap").step_out() end, desc = "Step Out" },
+    { "<leader>dO", function() require("dap").step_over() end, desc = "Step Over" },
+    { "<leader>dp", function() require("dap").pause() end, desc = "Pause" },
+    { "<leader>dr", function() require("dap").repl.toggle() end, desc = "Toggle REPL" },
+    { "<leader>ds", function() require("dap").session() end, desc = "Session" },
+    { "<leader>dt", function() require("dap").terminate() end, desc = "Terminate" },
+    { "<leader>dw", function() require("dap.ui.widgets").hover() end, desc = "Widgets" },
+  },
+  opts = function()
+    local dap = require("dap")
+    if not dap.adapters["pwa-node"] then
+      require("dap").adapters["pwa-node"] = {
         type = "server",
+        host = "localhost",
         port = "${port}",
         executable = {
-          command = vim.fn.stdpath("data") .. "/mason/bin/dlv",
-          args = { "dap", "-l", "127.0.0.1:${port}" },
+          command = "node",
+          -- 💀 Make sure to update this path to point to your installation
+          args = {
+            require("mason-registry").get_package("js-debug-adapter"):get_install_path()
+              .. "/js-debug/src/dapDebugServer.js",
+            "${port}",
+          },
         },
       }
-
-      vim.fn.sign_define("DapBreakpoint", { text = "🐞" })
-
-      local function dap_start_debugging()
-        dap.continue({})
-        vim.cmd("tabedit %")
-        vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<C-o>", false, true, true), "n", false)
-        ui.toggle({})
-      end
-
-      vim.keymap.set("n", "<leader>ds", dap_start_debugging)
-      vim.keymap.set("n", "<leader>dl", require("dap.ui.widgets").hover)
-      vim.keymap.set("n", "<leader>dc", dap.continue)
-      vim.keymap.set("n", "<leader>db", dap.toggle_breakpoint)
-      vim.keymap.set("n", "<leader>dn", dap.step_over)
-      vim.keymap.set("n", "<leader>di", dap.step_into)
-      vim.keymap.set("n", "<leader>do", dap.step_out)
-
-      local function dap_clear_breakpoints()
-        dap.clear_breakpoints()
-        --require("notify")("Breakpoints cleared", "warn")
-      end
-
-      vim.keymap.set("n", "<leader>dC", dap_clear_breakpoints)
-      vim.keymap.set("n", "<leader>dr", function()
-        require("dap").run_last()
-      end)
-
-      local function dap_end_debug()
-        dap.clear_breakpoints()
-        ui.toggle({})
-        dap.terminate({}, { terminateDebuggee = true }, function()
-          vim.cmd.bd()
-          --u.resize_vertical_splits()
-          -- require("notify")("Debugger session ended", "warn")
-        end)
-      end
-
-      vim.keymap.set("n", "<leader>de", dap_end_debug)
-
-      -- UI Settings
-      ui.setup({
-        icons = { expanded = "▾", collapsed = "▸" },
-        mappings = {
-          expand = { "<CR>", "<2-LeftMouse>" },
-          open = "o",
-          remove = "d",
-          edit = "e",
-          repl = "r",
-          toggle = "t",
-        },
-        layouts = {
+    end
+    for _, language in ipairs({ "typescript", "javascript", "typescriptreact", "javascriptreact" }) do
+      if not dap.configurations[language] then
+        dap.configurations[language] = {
           {
-            elements = {
-              "scopes",
-            },
-            size = 0.3,
-            position = "right",
+            type = "pwa-node",
+            request = "launch",
+            name = "Launch file",
+            program = "${file}",
+            cwd = "${workspaceFolder}",
           },
           {
-            elements = {
-              "repl",
-              "breakpoints",
-            },
-            size = 0.3,
-            position = "bottom",
+            type = "pwa-node",
+            request = "attach",
+            name = "Attach",
+            processId = require("dap.utils").pick_process,
+            cwd = "${workspaceFolder}",
           },
-        },
-        floating = {
-          -- max_height = nil,
-          -- max_width = nil,
-          border = "single",
-          mappings = {
-            close = { "q", "<Esc>" },
-          },
-        },
-        windows = { indent = 1 },
-        render = {
-          max_type_length = nil,
-        },
-      })
-    end,
-  },
+        }
+      end
+    end
+  end,
 }
