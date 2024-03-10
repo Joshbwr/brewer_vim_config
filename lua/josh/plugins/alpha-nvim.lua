c1a5fa8cb7275afed01f63f6a99476f2317edd02
Joshua Brewer <7298880+Joshbwr@users.noreply.github.com>
2024-03-10 10:54:29 -0500

    tweaks

--- lua/josh/plugins/alpha-nvim.lua
+++ lua/josh/plugins/alpha-nvim.lua
@@ -8,27 +8,17 @@ return {
 
     -- Set header
     dashboard.section.header.val = {
-      "                                                     ",
-      "  ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ",
-      "  ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ ",
-      "  ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ ",
-      "  ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ ",
-      "  ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ ",
-      "  ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ",
-      "                                                     ",
+      "NVIM v%s",
+      "Nvim is open source and freely distributable",
+      "https://neovim.io/#chat",
+      "type  :help nvim<Enter>       if you are new!",
+      "type  :checkhealth<Enter>     to optimize Nvim",
+      "type  :q<Enter>               to exit",
+      "type  :help<Enter>            for help",
+      "type  :help news<Enter> to see changes in v%s",
+      "Become a registered Vim user!",
+      "type  :help register<Enter>   for information]]",
     }
-
-    -- Set menu
-    dashboard.section.buttons.val = {
-      dashboard.button("e", "  > New File", "<cmd>ene<CR>"),
-      dashboard.button("SPC ee", "  > Toggle file explorer", "<cmd>NvimTreeToggle<CR>"),
-      dashboard.button("SPC ff", "󰱼 > Find File", "<cmd>Telescope find_files<CR>"),
-      dashboard.button("SPC fs", "  > Find Word", "<cmd>Telescope live_grep<CR>"),
-      dashboard.button("SPC wr", "󰁯  > Restore Session For Current Directory", "<cmd>SessionRestore<CR>"),
-      dashboard.button("q", " > Quit NVIM", "<cmd>qa<CR>"),
-    }
-
-    -- Send config to alpha
     alpha.setup(dashboard.opts)
 
     -- Disable folding on alpha buffer
